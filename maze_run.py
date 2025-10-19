094d634b2b0372fffffa6b9d2b2059e2d5664aa4
novi.li <novi.li@temu.com>
2025-10-19 20:20:32 +0800

    init: maze_run

---
+++ maze_run.py
@@ -0,0 +1,13 @@
+from util import *
+
+directions = ['North', 'East', 'South', 'West']
+
+def dfs():
+    pass
+
+def start(x, y, n):
+    move_to(x, y)
+    while True:
+
+        if get_entity_type == 'Treasure':
+            harvest()
